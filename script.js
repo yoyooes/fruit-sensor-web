e360ffb5a6a3729592e2ce1556b2c28713984bd5
yoyooes <premwit2008@gmail.com GitHub>
2025-10-28 15:09:31 +0700

    add button feature

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Fruit Sensor Web is working!");
+// ดึงปุ่มและพื้นที่แสดงผล
+const button = document.getElementById("myButton");
+const result = document.getElementById("result");
+
+// ตั้งค่าการคลิก
+button.addEventListener("click", () => {
+  result.textContent = "🎉 ปุ่มทำงานแล้ว!";
+  console.log("Button clicked!");
+});
